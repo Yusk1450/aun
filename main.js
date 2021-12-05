7a0228de610255786a4cd24d4f4ab22821cb05a0
Yusk <yusk1450@gmail.com>
2021-12-05 14:57:25 +0900

    dev

--- main.js
+++ main.js
@@ -30,6 +30,33 @@ function setup()
 
 	// fft = new p5.FFT();
 	// fft.setInput(mic);
+
+	$.ajax({
+		type: 'post',
+		url: 'http://yusk1450.pythonanywhere.com/',
+		data: {'name': '考えてるよ'},
+		async: false,
+		cache: false,
+		dataType: 'text',
+		scriptCharaset: 'utf-8'
+	})
+	.then(
+		function(data)
+		{
+			console.log(data);
+			setTimeout(function()
+			{
+				// var audio = new Audio('http://10.23.16.61:8888/Prototype/aun/output.mp3?cb=' + new Date().getTime());
+				// audio.load();
+				// audio.play();
+			}, 1000);
+		},
+		function(error)
+		{
+			console.log('error');
+			console.log(error);
+		}
+	);
 }
 
 function draw()
@@ -40,7 +67,7 @@ function draw()
 
 	let volume = mic.getLevel();
 
-	image(topImg, 0, 0, windowWidth, 166*(windowWidth/750));
+	image(topImg, 0, 0, windowWidth, 366*(windowWidth/750));
 
 	const faceImgSize = (windowWidth * 0.7) + ((windowWidth * 0.5) * volume * 1.2);
 	image(faceImg, windowWidth/2 - faceImgSize/2, windowHeight/2 - faceImgSize/2, faceImgSize, faceImgSize);
@@ -53,9 +80,9 @@ function draw()
 	// {
 	// 	image(quietlyImg, windowWidth/2 - 121/2, 448, 121, 127);
 	// }
-	image(quietlyImg, windowWidth/2 - 121/2, 448, 121, 127);
+	image(quietlyImg, windowWidth/2 - 121/2, 448, 121, 124);
 
-	image(bottomImg, 0, windowHeight-(414*(windowWidth/750)), windowWidth, 414*(windowWidth/750));
+	image(bottomImg, 0, windowHeight-(426*(windowWidth/750)), windowWidth, 426*(windowWidth/750));
 
 	// let spectrum = fft.analyze();
 }
@@ -72,6 +99,121 @@ function gotResult(error, results)
 	$('.result').html(results[0]['label']+'('+results[0]['confidence']+')').css('color', 'black');
 	if (results[0]['confidence'] > 0.85)
 	{
+		if (results[0]['label'] == 'バックグラウンド ノイズ')
+		{
+			return;
+		}
+
+		if (results[0]['label'] == 'think')
+		{
+			$.ajax({
+				type: 'post',
+				url: 'http://yusk1450.pythonanywhere.com/',
+				data: {'name': '考えてるよ'},
+				async: false,
+				cache: false,
+				dataType: 'text',
+				scriptCharaset: 'utf-8'
+			})
+			.then(
+				function(data)
+				{
+					setTimeout(function()
+					{
+						// var audio = new Audio('http://10.23.16.61:8888/Prototype/aun/output.mp3?cb=' + new Date().getTime());
+						// audio.load();
+						// audio.play();
+					}, 1000);
+				},
+				function(error)
+				{
+					console.log('error');
+					console.log(error);
+				}
+			);
+		}
+		else if (results[0]['label'] == 'question')
+		{
+			$.ajax({
+				type: 'post',
+				url: 'http://yusk1450.pythonanywhere.com/',
+				data: {'name': 'なに？'},
+				async: false,
+				cache: false,
+				dataType: 'text',
+				scriptCharaset: 'utf-8'
+			})
+			.then(
+				function(data)
+				{
+					setTimeout(function()
+					{
+						// var audio = new Audio('http://10.23.16.61:8888/Prototype/aun/output.mp3?cb=' + new Date().getTime());
+						// audio.load();
+						// audio.play();
+					}, 1000);
+				},
+				function()
+				{
+					console.log('error');
+				}
+			);
+		}
+		else if (results[0]['label'] == 'ok')
+		{
+			$.ajax({
+				type: 'post',
+				url: 'http://yusk1450.pythonanywhere.com/',
+				data: {'name': 'そうだね'},
+				async: false,
+				cache: false,
+				dataType: 'text',
+				scriptCharaset: 'utf-8'
+			})
+			.then(
+				function(data)
+				{
+					setTimeout(function()
+					{
+						// var audio = new Audio('http://10.23.16.61:8888/Prototype/aun/output.mp3?cb=' + new Date().getTime());
+						// audio.load();
+						// audio.play();
+					}, 1000);
+				},
+				function()
+				{
+					console.log('error');
+				}
+			);
+		}
+		else if (results[0]['label'] == 'please')
+		{
+			$.ajax({
+				type: 'post',
+				url: 'http://yusk1450.pythonanywhere.com/',
+				data: {'name': 'これ'},
+				async: false,
+				cache: false,
+				dataType: 'text',
+				scriptCharaset: 'utf-8'
+			})
+			.then(
+				function(data)
+				{
+					setTimeout(function()
+					{
+						// var audio = new Audio('http://10.23.16.61:8888/Prototype/aun/output.mp3?cb=' + new Date().getTime());
+						// audio.load();
+						// audio.play();
+					}, 1000);
+				},
+				function()
+				{
+					console.log('error');
+				}
+			);
+		}
+
 		console.log(results);
 		$('.result').html(results[0]['label']+'('+results[0]['confidence']+')').css('color', 'red');
 	}
